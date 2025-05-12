e1b5bfe744c3d204685a3a19b86044f46b9d26fa
Adrian Velasquez <141940483+adrianvelasquezs@users.noreply.github.com>
2025-05-12 15:25:33 -0500

    Add height sensor functionality in main.cpp and update readings logic

--- posture-sensor-app/src/main.cpp
+++ posture-sensor-app/src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
 
+// ============================================================
+// Posture
 int flexS = A0; // flex sensor is connected with pin A0 of the arduino
 int flexBuzzer = 5;
 int postureThreshold = 100;
 
+// ============================================================
+// Pulse
 int pulseS = A1; // pulse sensor is connected with pin A1 of the arduino
-int heightS = A2; // temperature sensor is connected with pin A2 of the arduino
+
+// ============================================================
+// Height
+int trigS = 9; // ultrasonic sensor trigger pin
+int echoS = 10; // ultrasonic sensor echo pin
+int const SENSOR_DEFAULT_HEIGHT = 200; // default height of the ultrasonic sensor
+
+// ============================================================
+// Sweat
 int sweatS = A3; // sweat sensor is connected with pin A3 of the arduino
 
+// ============================================================
+// READINGS
 int flexdata = 0;
 int pulsedata = 0;
 int heightdata = 0;
@@ -18,6 +32,8 @@ void setup()
     Serial.begin(9600);
     pinMode(flexS, INPUT);
     pinMode(flexBuzzer, OUTPUT);
+    pinMode(trigS, OUTPUT);
+    pinMode(echoS, INPUT);
 }
 
 void loop()
@@ -50,9 +66,18 @@ void loop()
     // Sensor de temperatura corporal
     // =======================================================
 
-    heightdata = analogRead(heightS);
+    digitalWrite(trigS, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigS, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigS, LOW);
+
+
+    const unsigned long duration = pulseIn(echoS, HIGH);
+
+    const float distance = SENSOR_DEFAULT_HEIGHT - ( ( duration * 0.034 ) / 2 );
     Serial.println("height value;"
-        + String(heightdata) );
+        + String( distance ) );
 
     // =======================================================
     // Sensor de sudor
